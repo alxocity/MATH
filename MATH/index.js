63336a7c612b27ce59d9116c000101e42d7de421
Alex Hansen <alxo@cryptokitties.city>
2020-01-04 00:50:21 -0800

    Remove text-to-svg

--- MATH/index.js
+++ MATH/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     const { id } = req.query;
     context.res = {
         body: {
-            image_data: require('text-to-svg').loadSync().getSVG(id),
+            image_data: `<svg width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="${Math.ceil(350 / id.length)}px">${id}</text></svg>`,
             external_url: `https://etherscan.io/token/0x6b4fccdd888bb6fd3934a9e49ef64dfd2c0d8e6d?a=${id}`,
             name: id
         }
