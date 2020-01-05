743328caaab1bc146523b6664508c67533c4ff9a
Alex Hansen <alxo@cryptokitties.city>
2020-01-04 18:44:44 -0800

    Remove redundant name from metadata

--- MATH/index.js
+++ MATH/index.js
@@ -3,8 +3,7 @@ module.exports = async function (context, req) {
     context.res = {
         body: {
             image_data: `<svg width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="${Math.ceil(350 / id.length)}px">${id}</text></svg>`,
-            external_url: `https://etherscan.io/token/0x6b4fccdd888bb6fd3934a9e49ef64dfd2c0d8e6d?a=${id}`,
-            name: id
+            external_url: `https://etherscan.io/token/0x6b4fccdd888bb6fd3934a9e49ef64dfd2c0d8e6d?a=${id}`
         }
     };
 };
