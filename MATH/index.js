158cb207785c2ad17b807e5cbc910db2ca738a7a
alxo#6661 <alxo@cryptokitties.city>
2020-01-08 12:12:19 -0800

    Add xmlns attribute in image metadata

--- MATH/index.js
+++ MATH/index.js
@@ -6,7 +6,7 @@ module.exports = async function (context, req) {
   const color = n => n.toString(16).padStart(6, '0');
   context.res = {
     body: {
-      image_data: `<svg width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="${Math.ceil(350 / id.length)}px" fill="#${color(bigInt % 16777216n)}">${id}</text></svg>`,
+      image_data: `<svg xmlns="http://www.w3.org/2000/svg" width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="${Math.ceil(350 / id.length)}px" fill="#${color(bigInt % 16777216n)}">${id}</text></svg>`,
       external_url: `https://etherscan.io/token/0x6b4fccdd888bb6fd3934a9e49ef64dfd2c0d8e6d?a=${id}`,
       description: bigInt.toString(2).padStart(256, '0'),
       name: id,
