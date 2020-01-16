83043f8a38c0f4836bf5c601fe78380fccdcf7bf
Alex Hansen <alxo@cryptokitties.city>
2020-01-16 00:14:52 -0800

    Add emoji encoding to description in api

--- MATH/index.js
+++ MATH/index.js
@@ -3,18 +3,19 @@ module.exports = async function (context, req) {
   const digits = Array.from(Array(10), (x, i) => (id.match(new RegExp(i, 'g')) || []).length);
   const sum = digits.reduce((sum, x, i) => sum + x * i, 0);
   const number = BigInt(id);
-  const colorMax = BigInt('0xffffff');
+  const colorMax = BigInt(0xffffff);
   const colorNumber = number % (colorMax + 1n);
-  const color = n => n.toString(16).padStart(6, '0');
+  const color = n => n.toString(16).padStart(6, 0);
   context.res = {
     body: {
       image_data: `<svg xmlns="http://www.w3.org/2000/svg" width="350" height="350"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="${Math.ceil(350 / id.length)}px" fill="#${color(colorNumber)}">${id}</text></svg>`,
       external_url: `https://etherscan.io/token/0x6b4fccdd888bb6fd3934a9e49ef64dfd2c0d8e6d?a=${id}`,
       description: [
-        `0x${number.toString(16).padStart(64, '0')}`,
-        number.toString(2).padStart(256, '0').replace(/0/g, '⬛').replace(/1/g, '⬜').match(/.{16}/g).join('\n'),
-        number.toString(16).padStart(64, '0').match(/.{2}/g).map(x => String.fromCharCode(`0x${x}`)).map(x => JSON.stringify(x).length == 3 ? x : '').join(''),
-        number.toString(16).padStart(64, '0').match(/.{4}/g).map(x => String.fromCharCode(`0x${x}`)).map(x => JSON.stringify(x).length == 3 ? x : '').join('')
+        `0x${number.toString(16).padStart(64, 0)}`,
+        number.toString(2).padStart(256, 0).replace(/0/g, '⬛').replace(/1/g, '⬜').match(/.{16}/g).join('\n'),
+        number.toString(16).padStart(64, 0).match(/.{2}/g).map(x => String.fromCodePoint(`0x${x}`)).map(x => JSON.stringify(x).length == 3 ? x : '').join(''),
+        number.toString(16).padStart(64, 0).match(/.{4}/g).map(x => String.fromCodePoint(`0x${x}`)).map(x => JSON.stringify(x).length == 3 ? x : '').join(''),
+        number.toString(16).padStart(64, 0).match(/.{8}/g).map(x => `0x${x}` <= 0x10FFFF ? String.fromCodePoint(`0x${x}`) : '').map(x => JSON.stringify(x).length == 4 ? x : '').join('')
       ].join('\n\n'),
       name: id,
       attributes: [
@@ -39,7 +40,7 @@ module.exports = async function (context, req) {
           trait_type: 'fancy',
           value: 'palindromic'
         }] : []),
-        ...(id == id.split('').reverse().join('').replace(/6/g, '-').replace(/9/g, '6').replace(/-/g, '9').replace(/2|3|4|5|7/g, '') ? [{
+        ...(id == id.split('').reverse().join('').replace(/6/g, '-').replace(/9/g, 6).replace(/-/g, 9).replace(/2|3|4|5|7/g, '') ? [{
           trait_type: 'fancy',
           value: 'strobogrammatic'
         }] : [])
