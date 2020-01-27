2a6032f2545491a1e26ada1e6cbaebf5bd1bdac5
Alex Hansen <alxo@cryptokitties.city>
2020-01-26 20:05:52 -0800

    Add face text and tone color to API

--- TOON/index.js
+++ TOON/index.js
@@ -17,6 +17,16 @@ const faceContract = new web3.eth.Contract([{
   name: 'getFace',
   outputs: [{ name: 'face', type: 'string' }],
   type: 'function'
+},{
+  inputs: [{ name: 'id', type: 'uint256' }],
+  name: 'getTextColor',
+  outputs: [{ name: 'textColor', type: 'uint256' }],
+  type: 'function'
+},{
+  inputs: [{ name: 'id', type: 'uint256' }],
+  name: 'getBackgroundColor',
+  outputs: [{ name: 'backgroundColor', type: 'uint256' }],
+  type: 'function'
 }], '0x91047abf3cab8da5a9515c8750ab33b4f1560a7a');
 const rgbContract = new web3.eth.Contract([{
   inputs: [{ name: 'id', type: 'uint256' }],
@@ -29,12 +39,15 @@ module.exports = async function (context, req) {
   const { word, face, rgb } = await contract.methods.get(id).call();
   const name = await wordContract.methods.getWord(word).call();
   const faceValue = await faceContract.methods.getFace(face).call();
+  const color = n => Number(n).toString(16).padStart(6, 0);
+  const faceTextColor = color(await faceContract.methods.getTextColor(face).call());
+  const faceToneColor = color(await faceContract.methods.getBackgroundColor(face).call());
   const { r, g, b } = await rgbContract.methods.get(rgb).call();
   const bits = x => BigInt(x).toString(2).padStart(256, 0).replace(/1/g, 'f');
   const rBits = bits(r), gBits = bits(g), bBits = bits(b);
   context.res = {
     body: {
-      image_data: `<svg xmlns="http://www.w3.org/2000/svg" width="350" height="350">${Array(256).fill().reduce((a, _, i) => `${a}<rect x="${79 + 12 * (i % 16)}" y="${47 + 12 * Math.floor(i / 16)}" width="12" height="12" fill="#${rBits[i]}${gBits[i]}${bBits[i]}"/>`, '')}<text x="50%" y="271" dominant-baseline="middle" text-anchor="middle" font-size="72px">${faceValue}</text></svg>`,
+      image_data: `<svg xmlns="http://www.w3.org/2000/svg" width="350" height="350"><rect x="47" y="235" width="256" height="72" fill="#${faceToneColor}"/>${Array(256).fill().reduce((a, _, i) => `${a}<rect x="${79 + 12 * (i % 16)}" y="${47 + 12 * Math.floor(i / 16)}" width="12" height="12" fill="#${rBits[i]}${gBits[i]}${bBits[i]}"/>`, '')}<text x="50%" y="271" dominant-baseline="middle" text-anchor="middle" font-size="72px" fill="#${faceTextColor}">${faceValue}</text></svg>`,
       external_url: `https://etherscan.io/token/${address}?a=${id}`,
       name,
       attributes: [{ trait_type: 'word', value: word }, { trait_type: 'face', value: face }, { trait_type: 'rgb', value: rgb }]
